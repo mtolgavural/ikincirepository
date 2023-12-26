6cb7e72a3e00dce532323d5b3302afad20f3c5a9
Tolga VURAL <m.tolgavural@hotmail.com>
2023-12-26 22:14:29 +0300

    giriş yazısı tugba tarafından değiştirildi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy Github Kursuna Hoşgeldiniz !
+          Git Github Öğreniyorum w/tugba
         </p>
         <a
           className="App-link"
